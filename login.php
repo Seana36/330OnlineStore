1ce917f08cdf9405106aef1226be1e2d0404489b
misterSchmaltz <angeloscastle02@gmail.com>
2016-11-15 12:22:05 -0500

    Login Update 2
    Seperated the login into an html, js, and php file.

--- login.php
+++ login.php
@@ -1,155 +1,44 @@
-<html lang = "en">
+<?php 
+require "dbConnect.php";
 
-	<head>
-		<title>Wham-Bham-Azon Login</title>
-		<link href = "css/bootstrap.min.css" rel = "stylesheet">
-		<link href="css/shop-homepage.css" rel="stylesheet">
-		<style>
-         body {
-            padding-top: 40px;
-            padding-bottom: 40px;
-         }
-         
-         .form-signin {
-            max-width: 330px;
-            padding: 15px;
-            margin: 0 auto;
-            color: #017572;
-         }
-         
-         .form-signin .form-signin-heading,
-         .form-signin .checkbox {
-            margin-bottom: 10px;
-         }
-         
-         .form-signin .checkbox {
-            font-weight: normal;
-         }
-         
-         .form-signin .form-control {
-            position: relative;
-            height: auto;
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-            padding: 10px;
-            font-size: 16px;
-         }
-         
-         .form-signin .form-control:focus {
-            z-index: 2;
-         }
-         
-         .form-signin input[type="email"] {
-            margin-bottom: -1px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
-            border-color:#017572;
-         }
-         
-         .form-signin input[type="password"] {
-            margin-bottom: 10px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-            border-color:#017572;
-         }
-         
-         h2{
-            text-align: center;
-            color: #017572;
-         }
-      </style>
-      
-   </head>
-	
-   <body>
-      
-      <h2>Enter Username and Password</h2>
-	   
-      <div class = "container form-signin">
-         
-         <?php
-            $msg = '';
-            
-            $servername = "localhost";
-			$username = "root";
-			$password = "";
-			$dbname = "StoreDatabase";
-			
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			
-			if($conn->connect_error) {
-				die("Connection failed: " . $connect->connect_error);
-			}
-			else {
-				echo "";
-			}
-			
-         ?>
-      </div> <!-- /container -->
-      
-	  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-					<li>
-						<a href="login.php">Login/Signup</a>
-					</li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-	  
-      <div class = "container">
-      
-         <form class = "form-signin" role = "form" 
-            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
-            ?>" method = "post">
-            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
-            <input type = "text" class = "form-control" 
-               name = "username" placeholder = "username" 
-               required autofocus></br>
-            <input type = "password" class = "form-control"
-               name = "password" placeholder = "password" required>
-            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
-               name = "login">Login</button>
-         </form>
-		 
-		 <form class = "form-signup" role = "form"
-			action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
-			?>" method = "post">
-         Click here to clean <a href = "logout.php" tite = "Logout">Session.
-         
-      </div> 
-	  
-	  
-	  <!-- jQuery -->
-		<script src="js/jquery.js"></script>
+$username = $_POST["username"];
 
-		<!-- Bootstrap Core JavaScript -->
-		<script src="js/bootstrap.min.js"></script>
-	
-	</body>
-</html>
+$sql = "SELECT position, fName, lName, employeeID FROM employee WHERE employeeID = (SELECT employeeID FROM account WHERE username = '$username');"; 
+
+$result = $conn->query($sql);
+if (!$result){
+	die("query failed" . $conn->error);
+}
+$entry = $result->fetch_row();
+$position = $entry[0];
+$_SESSION["user"] = $entry[1] . " " . $entry[2];
+$_SESSION["userID"] = $entry[3];
+
+switch($position){
+	case "Product Manager":
+		echo "Location:pmindex.html";
+		break;
+	default:
+		echo "You've entered an incorrect username or password!";
+}
+
+$sql = "SELECT position, fName, lName, customerID FROM customer WHERE customerID = (SELECT customerID FROM customer WHERE username = '$username');"; 
+
+$result = $conn->query($sql);
+if (!$result){
+	die("query failed" . $conn->error);
+}
+$entry = $result->fetch_row();
+$position = $entry[0];
+$_SESSION["user"] = $entry[1] . " " . $entry[2];
+$_SESSION["userID"] = $entry[3];
+
+switch($position){
+	case "NULL":
+		echo "Location:index.html";
+		echo "Login Successful";
+		break;
+	default:
+		echo "You've entered an incorrect username or password!";
+}
+?> 
