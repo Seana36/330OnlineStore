322f65aa3d201d836045ffdcc2c1d9dc5152f8f2
Seana36 <sean.emily11@gmail.com>
2016-11-15 21:37:56 -0500

    adding stuff back in

---
+++ products.php
@@ -0,0 +1,28 @@
+<?php
+
+/* products.php
+Stores information for products
+*/
+
+// Define product information
+$products = array(
+	1 => array(
+		'name' => 'Hat',
+		'price' => 24.99,
+		'category' => 'Accessories',
+		'description' => 'Classic hat. 100% Polyester'
+		    ),
+	2 => array(
+		'name' => 'Shirt',
+		'price' => 49.99,
+		'category' => 'Tops',
+		'description' => 'Crew neck shirt. 100% Cotton'
+		    ),
+	3 => array(
+		'name' => 'Pants',
+		'price' => 99.99,
+		'category' => 'Bottoms',
+		'description' => 'Woven pants. 100% Nylon'
+		     )
+		      );
+?>
