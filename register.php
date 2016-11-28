bf3a0291b2afe8c11d97ef4167986b24f1d9fd08
misterSchmaltz <angeloscastle02@gmail.com>
2016-11-28 16:33:26 -0500

    Updates to Login, implementation of Register
    Added an html and php file for register, couldn't get the new users
    added to the database however.

---
+++ register.php
@@ -0,0 +1,33 @@
+<?php
+require "dbConnect.php";
+
+$username = $_POST["username"];
+$fName = $_POST["fName"];
+$lName = $_POST["lName"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+$phoneNo = $_POST["phoneNo"];
+$securityQuestion = $_POST["securityQuestion"];
+$securityQuestionAns = $_POST["securityQuestionAns"];
+
+
+$sql = "SELECT position, fName, lName, customerID FROM customer WHERE customerID = (SELECT customerID FROM customer WHERE username = '$username');";
+
+$result = $conn->query($sql);
+
+if (!$result){
+	die("query failed" . $conn->error);
+}
+$entry = $result->fetch_row();
+if($entry)
+{
+	echo "User already exists";
+}
+
+else
+{
+	$query = "INSERT INTO customer(fName, lName, userName, password, email, phoneNo, securityQuestion, securityQuestionAns) VALUES('$fName', '$lName', '$userName', '$password', '$email', '$phoneNo', '$securityQuestion', '$securityQuestionAns'";
+	$res = mysql_query($query);
+}
+
+?>
