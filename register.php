c6c73391dd6cd74a1fb9f31e46766f23051ae05a
Seana36 <sean.emily11@gmail.com>
2016-11-29 16:32:58 -0500

    fixed insert, not sure how tho

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 require "./includes/dbConnect.php";
 
-$username = $_POST["username"];
+$userName = $_POST["username"];
 $fName = $_POST["fName"];
 $lName = $_POST["lName"];
 $password = $_POST["password"];
@@ -11,7 +11,7 @@ $securityQuestion = $_POST["securityQuestion"];
 $securityQuestionAns = $_POST["securityQuestionAns"];
 
 
-$sql = "SELECT position, fName, lName, customerID FROM customer WHERE customerID = (SELECT customerID FROM customer WHERE username = '$username');";
+$sql = "SELECT fname, lname, customerID FROM customer WHERE customerID = (SELECT customerID FROM customer WHERE userName = '$userName');";
 
 $result = $conn->query($sql);
 
@@ -26,8 +26,25 @@ if($entry)
 
 else
 {
-	$query = "INSERT INTO customer(fName, lName, userName, password, email, phoneNo, securityQuestion, securityQuestionAns) VALUES('$fName', '$lName', '$userName', '$password', '$email', '$phoneNo', '$securityQuestion', '$securityQuestionAns'";
-	$res = mysql_query($query);
+	#INSERT INTO `customer`(`customerID`, `fName`, `lName`, `userName`, `password`, `email`, `phoneNo`, `secuirtyQuestion`, `secuirtyQuestionAns`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9])
+	$query = "INSERT INTO`customer`( `fName`, `lName`, `userName`, `password`, `email`, `phoneNo`, `secuirtyQuestion`, `secuirtyQuestionAns`) VALUES('$fName', '$lName', '$userName', '$password', '$email', $phoneNo, '$securityQuestion', '$securityQuestionAns')";
+	#INSERT INTO `customer`( `fName`, `lName`, `userName`, `password`, `email`, `phoneNo`, `secuirtyQuestion`, `secuirtyQuestionAns`) VALUES ('hello','hello','hello','hello','hello',123,'hello','hello')
+#echo $userName . " 1<br>" ;
+#echo $fName. " 2<br>" ;;
+#echo $lName . " 3<br>" ;
+#echo $password . " 4<br>" ;
+#echo $email . " 5<br>" ;
+#echo $phoneNo. " 6<br>" ;
+#echo $securityQuestion . " 7<br>" ;
+#echo $securityQuestionAns . " 8<br>" ;
+	#$res = $conn->query($query);
+	if (!$conn->query($query)){
+	die("query1 failed:" . $conn->error);
+}
+#	// if(!$res)
+#	// 	echo "Insert Query Failed";
+#	// else
+#	// 	echo "User added";
 }
 
 ?>
