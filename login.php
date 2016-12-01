a4444c20d81fdb41c9c2e8ad80049020218d15eb
Seana36 <sean.emily11@gmail.com>
2016-12-01 11:01:51 -0500

    login semi working

--- login.php
+++ login.php
@@ -2,43 +2,20 @@
 require "./includes/dbConnect.php";
 
 $username = $_POST["username"];
+$password = $_POST["password"]; 
 
-$sql = "SELECT position, fName, lName, employeeID FROM employee WHERE employeeID = (SELECT employeeID FROM account WHERE username = '$username');"; 
-
-$result = $conn->query($sql);
-if (!$result){
-	die("query failed" . $conn->error);
-}
-$entry = $result->fetch_row();
-$position = $entry[0];
-$_SESSION["user"] = $entry[1] . " " . $entry[2];
-$_SESSION["userID"] = $entry[3];
-
-switch($position){
-	case "Product Manager":
-		echo "Location:pmindex.html";
-		break;
-	default:
-		echo "You've entered an incorrect username or password!";
-}
-
-$sql = "SELECT position, fName, lName, customerID FROM customer WHERE customerID = (SELECT customerID FROM customer WHERE username = '$username');"; 
+$sql = "SELECT  fName, lName, customerID FROM customer WHERE customerID = (SELECT customerID FROM customer WHERE username = '$username' AND password ='$password');"; 
 
 $result = $conn->query($sql);
 if (!$result){
 	die("query failed" . $conn->error);
 }
 $entry = $result->fetch_row();
-$position = $entry[0];
-$_SESSION["user"] = $entry[1] . " " . $entry[2];
-$_SESSION["userID"] = $entry[3];
+#$position = $entry[0];
+#echo $entry[3];
+ #$_SESSION["name"] = $entry[1] . " " . $entry[2];
+ #$_SESSION["customer"] = $entry[3];
+#echo "Location:homeController.php";
 
-switch($position){
-	case "NULL":
-		echo "Location:index.html";
-		echo "Login Successful";
-		break;
-	default:
-		echo "You've entered an incorrect username or password!";
-}
-?> 
+
+?>
